e9bd88d490fb485f43fa6d31ae1654eb97d491bc
Vincent Knauss <vbk737@gmail.com>
2022-02-17 02:19:04 -0600

    + and - work

--- src/console_thread.cpp
+++ src/console_thread.cpp
@@ -112,7 +112,92 @@ bool BaseToken::match(Token token, T& value) const {
     return false;
 }
 
-bool matchStrings(const std::string& compared, const std::initializer_list<std::string>& comparing, size_t start, size_t end) {
+class BaseExpression {
+
+public:
+
+    virtual std::unique_ptr<BaseExpression> evaluate() const = 0;
+
+    virtual ~BaseExpression() { }
+
+    template<typename T>
+    bool match(T& value) const;
+
+};
+
+template<typename T>
+class ValueExpression : public BaseExpression {
+public:
+
+    ValueExpression(const T& value) : _value(value) { }
+
+    const T& value() const {
+        return _value;
+    }
+
+    std::unique_ptr<BaseExpression> evaluate() const override {
+        return std::make_unique<ValueExpression<T>>(*this);
+    }
+
+private:
+
+    T _value;
+};
+
+template<typename Op>
+class UnaryExpression : public BaseExpression {
+public:
+
+    UnaryExpression(const Op& op, std::unique_ptr<BaseExpression>&& expr) :
+        _op(op), _expr(std::move(expr)) { }
+
+    std::unique_ptr<BaseExpression> evaluate() const override {
+        return std::unique_ptr<BaseExpression>(_op(*_expr->evaluate()));
+    }
+
+private:
+
+    Op _op;
+
+    std::unique_ptr<BaseExpression> _expr;
+
+};
+
+template<typename Op>
+class BinaryExpression : public BaseExpression {
+public:
+
+    BinaryExpression(const Op& op, std::unique_ptr<BaseExpression>&& expr1,
+        std::unique_ptr<BaseExpression>&& expr2) :
+            _op(op), _expr1(std::move(expr1)), _expr2(std::move(expr2)) { }
+
+    std::unique_ptr<BaseExpression> evaluate() const override {
+        return std::unique_ptr<BaseExpression>(_op(*_expr1->evaluate(), *_expr2->evaluate()));
+    }
+
+private:
+
+    Op _op;
+
+    std::unique_ptr<BaseExpression> _expr1, _expr2;
+
+};
+
+enum class ControlFlowSymbol {
+    EXIT
+};
+
+template<typename T>
+bool BaseExpression::match(T& value) const {
+    if (auto ve = dynamic_cast<const ValueExpression<T>*>(this)) {
+        value = ve->value();
+        return true;
+    }
+    return false;
+}
+
+static bool matchStrings(const std::string& compared,
+        const std::initializer_list<std::string>& comparing, size_t start, size_t end) {
     for (const std::string& s : comparing) {
         if (compared.compare(start, end, s) == 0) {
             return true;
@@ -121,7 +206,8 @@ bool matchStrings(const std::string& compared, const std::initializer_list<std::
     return false;
 }
 
-std::tuple<std::unique_ptr<BaseToken>, size_t> getToken(const std::string& line, size_t position) {
+static std::tuple<std::unique_ptr<BaseToken>, size_t>
+getToken(const std::string& line, size_t position) {
     // skip beginning whitespace
     while (position < line.length() && isspace(line[position])) ++position;
     if (position == line.length()) {
@@ -238,7 +324,8 @@ std::tuple<std::unique_ptr<BaseToken>, size_t> getToken(const std::string& line,
     return make_value_ret(Token::VALUE_STRING, line.substr(position, endpos-position), endpos);
 }
 
-std::vector<std::unique_ptr<BaseToken>> getLineTokens(const std::string& line) {
+static std::vector<std::unique_ptr<BaseToken>>
+getLineTokens(const std::string& line) {
     std::vector<std::unique_ptr<BaseToken>> tokens;
 
     size_t position = 0;
@@ -252,89 +339,6 @@ std::vector<std::unique_ptr<BaseToken>> getLineTokens(const std::string& line) {
     return tokens;
 }
 
-class BaseExpression {
-
-public:
-
-    virtual std::unique_ptr<BaseExpression> evaluate() const = 0;
-
-    virtual ~BaseExpression() { }
-
-    template<typename T>
-    bool match(T& value) const;
-
-};
-
-template<typename T>
-class ValueExpression : public BaseExpression {
-public:
-
-    ValueExpression(const T& value) : _value(value) { }
-
-    const T& value() const {
-        return _value;
-    }
-
-    std::unique_ptr<BaseExpression> evaluate() const override {
-        return std::make_unique<ValueExpression<T>>(*this);
-    }
-
-private:
-
-    T _value;
-};
-
-template<typename Op>
-class UnaryExpression : public BaseExpression {
-public:
-
-    UnaryExpression(const Op& op, std::unique_ptr<BaseExpression>&& expr) :
-        _op(op), _expr(std::move(expr)) { }
-
-    std::unique_ptr<BaseExpression> evaluate() const override {
-        return std::make_unique<BaseExpression>(_op(*_expr->evaluate()));
-    }
-
-private:
-
-    Op _op;
-
-    std::unique_ptr<BaseExpression> _expr;
-
-};
-
-template<typename Op>
-class BinaryExpression : public BaseExpression {
-public:
-
-    BinaryExpression(const Op& op, std::unique_ptr<BaseExpression>&& expr1, std::unique_ptr<BaseExpression>&& expr2) :
-        _op(op), _expr1(std::move(expr1)), _expr2(std::move(expr2)) { }
-
-    std::unique_ptr<BaseExpression> evaluate() const override {
-        return std::make_unique<BaseExpression>(_op(*_expr1->evaluate(), *_expr2->evaluate()));
-    }
-
-private:
-
-    Op _op;
-
-    std::unique_ptr<BaseExpression> _expr1, _expr2;
-
-};
-
-enum class ControlFlowSymbol {
-    EXIT
-};
-
-template<typename T>
-bool BaseExpression::match(T& value) const {
-    if (auto ve = dynamic_cast<const ValueExpression<T>*>(this)) {
-        value = ve->value();
-        return true;
-    }
-    return false;
-}
-
 template<typename T>
 static std::string as_string(const T& t) {
     return std::to_string(t);
@@ -345,9 +349,8 @@ std::string as_string(const std::string& s) {
     return s;
 }
 
-std::unique_ptr<BaseExpression> parseTokens(const std::vector<std::unique_ptr<BaseToken>>& tokens) {
-    std::unique_ptr<BaseExpression> expr = nullptr;
-
+static std::tuple<std::unique_ptr<BaseExpression>, size_t>
+buildExpression(std::unique_ptr<BaseExpression>&& left, const std::vector<std::unique_ptr<BaseToken>>& tokens, size_t position) {
     auto unexpected_token_type = [] (Token token, auto value) {
         return std::runtime_error("Unexpected token " + tokenString(token) + ": " + as_string(value));
     };
@@ -360,54 +363,149 @@ std::unique_ptr<BaseExpression> parseTokens(const std::vector<std::unique_ptr<Ba
         return std::make_unique<ValueExpression<decltype(value)>>(value);
     };
 
-    auto handle_value_case = [&] (auto& token, auto& value) {
-        assert(token->match(token->token(), value));
-        if (expr) {
-            throw unexpected_token_type(token->token(), value);
-        }
-        expr = make_value_expr(value);
+    auto make_binary_expr = [] (auto op, auto&& left, auto&& right) {
+        return std::make_unique<BinaryExpression<decltype(op)>>(op, std::move(left), std::move(right));
     };
 
-    for (auto i = 0u; i < tokens.size(); ++i) {
-        switch (tokens[i]->token()) {
-            case Token::VALUE_FLOAT: {
-                float value;
-                handle_value_case(tokens[i], value);
-                break;
-            }
-            case Token::VALUE_INT: {
-                int value;
-                handle_value_case(tokens[i], value);
-                break;
-            }
-            case Token::VALUE_STRING: {
-                std::string value;
-                handle_value_case(tokens[i], value);
-                break;
-            }
-            case Token::EXIT: {
-                if (expr) {
-                    throw unexpected_token(tokens[i]->token());
-                }
-                expr = make_value_expr(ControlFlowSymbol::EXIT);
-                break;
-            }
-            case Token::PLUS: {
-                if (!expr) {
-                    throw unexpected_token(tokens[i]->token());
-                }
-                
-            }
+    auto make_unary_expr = [] (auto op, auto&& e) {
+        return std::make_unique<UnaryExpression<decltype(op)>>(op, std::move(e));
+    };
 
-            default:
-                throw unexpected_token(tokens[i]->token());
+    auto make_base_expr = [] (auto&& expr) {
+        return std::unique_ptr<BaseExpression>(std::move(expr));
+    };
+
+    auto make_ret = [] (auto&& expr, size_t position) {
+        return std::make_tuple(std::move(expr), position);
+    };
+
+    auto handle_value_case = [&] (const auto& token, auto& value) {
+        assert(token->match(token->token(), value));
+        if (left) {
+            throw unexpected_token_type(token->token(), value);
         }
+        return make_ret(make_value_expr(value), position + 1);
+    };
+
+    auto make_type_op = [&] (auto op, auto lval, const BaseExpression& rexp) {
+        if (decltype(lval) rval; rexp.match(rval)) {
+            return make_base_expr(make_value_expr(op(lval, rval)));
+        } else {
+            throw std::runtime_error("Types for binary operation must match");
+        }
+    };
+
+    auto handle_binary_case = [&] (const auto& token, auto op) {
+        if (!left) {
+            throw unexpected_token(token->token());
+        }
+        auto [right, nextpos] = buildExpression(nullptr, tokens, position+1);
+        if (!right) {
+            throw std::runtime_error("Expected expression for right hand side of token " + tokenString(token->token()));
+        }
+        return make_ret(make_binary_expr(op, std::move(left), std::move(right)), nextpos);
+    };
+    
+    auto handle_unary_case = [&] (const auto& token, auto op) {
+        if (left) {
+            throw unexpected_token(token->token());
+        }
+        auto [right, nextpos] = buildExpression(nullptr, tokens, position+1);
+        if (!right) {
+            throw std::runtime_error("Expected expression for right hand side of token " + tokenString(token->token()));
+        }
+        return make_ret(make_unary_expr(op, std::move(right)), nextpos);
+    };
+
+    if (position == tokens.size()) {
+        return make_ret(nullptr, position);
     }
+
+    const auto& ptok = tokens[position];
+    switch (ptok->token()) {
+        case Token::VALUE_FLOAT: {
+            float value;
+            return handle_value_case(ptok, value);
+        }
+        case Token::VALUE_INT: {
+            int value;
+            return handle_value_case(ptok, value);
+        }
+        case Token::VALUE_STRING: {
+            std::string value;
+            return handle_value_case(ptok, value);
+        }
+        case Token::EXIT: {
+            if (left) {
+                throw unexpected_token(ptok->token());
+            }
+            return make_ret(make_value_expr(ControlFlowSymbol::EXIT), position+1);
+        }
+        case Token::PLUS: {
+            auto op = [] (auto x, auto y) { return x + y; };
+            return handle_binary_case(ptok,
+                [&] (const BaseExpression& e1, const BaseExpression& e2) {
+                    if (int x; e1.match(x)) {
+                        return make_type_op(op, x, e2);
+                    }
+                    if (float x; e1.match(x)) {
+                        return make_type_op(op, x, e2);
+                    }
+                    if (std::string x; e1.match(x)) {
+                        return make_type_op(op, x, e2);
+                    }
+                    throw std::runtime_error("Expected value for left hand side of binary op");
+                });
+        }
+        case Token::MINUS: {
+            if (left) {
+                auto op = [] (auto x, auto y) { return x - y; };
+                return handle_binary_case(ptok,
+                    [&] (const BaseExpression& e1, const BaseExpression& e2) {
+                        if (int x; e1.match(x)) {
+                            return make_type_op(op, x, e2);
+                        }
+                        if (float x; e1.match(x)) {
+                            return make_type_op(op, x, e2);
+                        }
+                        throw std::runtime_error("Expected value for left hand side of binary op");
+                    });
+            } else {
+                auto op = [] (auto x) { return -x; };
+                return handle_unary_case(ptok, [&] (const BaseExpression& e) {
+                    if (int x; e.match(x)) {
+                        return make_base_expr(make_value_expr(op(x)));
+                    }
+                    if (float x; e.match(x)) {
+                        return make_base_expr(make_value_expr(op(x)));
+                    }
+                    throw std::runtime_error("Expected value for right hand side of unary op");
+                });
+            }
+        }
+
+        default:
+            throw unexpected_token(ptok->token());
+    }
+}
+
+static std::unique_ptr<BaseExpression>
+parseTokens(const std::vector<std::unique_ptr<BaseToken>>& tokens) {
+    std::unique_ptr<BaseExpression> expr = nullptr;
+
+    size_t position = 0;
+    while (position < tokens.size()) {
+        auto [newexpr, nextpos] = buildExpression(std::move(expr), tokens, position);
+        expr = std::move(newexpr);
+        position = nextpos;
+    }
+    
     return expr;
 }
 
 static void consoleThreadMain(IOStreamWrapper streams) {
     bool running = true;
+
     while (running) {
         streams.out << ">> " << std::flush;
         if (std::string line; getline(streams.in, line)) {
